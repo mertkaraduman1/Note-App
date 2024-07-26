7c58088fa76d906635d1388d8f1d8fd18f24d641
mertkaraduman <braumfly@hotmail.com>
2024-07-26 11:02:07 +0300

    Initial Commit

---
+++ Note-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Note-App
+//
+//  Created by Mert Karaduman on 26.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
